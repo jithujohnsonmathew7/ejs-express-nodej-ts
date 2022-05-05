301b4f8d81a2d5fc98a10b543040a0d37e06bd24
jithujohnsonmathew7 <74446770+jithujohnsonmathew7@users.noreply.github.com>
2022-05-05 08:57:44 +0530

    update

--- dist/src/controllers/error.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Error {
-}
-Error.Index = (req, res, next) => {
-    res.render('pages/error');
-};
-exports.default = Error;
